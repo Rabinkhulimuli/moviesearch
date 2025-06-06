ef7e50ebeae844232754316319428aa617409023
Rabinkhulimuli <131673248+Rabinkhulimuli@users.noreply.github.com>
2025-06-06 07:28:03 +0545

    some change

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -7,8 +7,8 @@ export default function NotFound({
 }) {
   return (
     <div className="flex relative items-center justify-center h-screen">
-      <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-[4rem] backdrop-blur-md">
-        <h1>{msg}</h1>
+      <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-xl text-red-100 md:text-[4rem] backdrop-blur-md">
+        <h1 className="uppercase">{msg}</h1>
         <h2 className="text-red-700 text-xl">{error} </h2>
       </div>
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ import Loading from "../Loading";
 export default function Layout() {
   return (
     <div className="bg-black text-white h-full">
-      <div className="fixed z-15 inset-0 top-0 bg-white/20 h-fit md:px-4 lg:px-8">
+      <div className="fixed z-40 inset-0 top-0 bg-white/20 backdrop-blur-md h-fit md:px-4 lg:px-8">
         <Header />
       </div>
       <Suspense fallback={<Loading />}>
